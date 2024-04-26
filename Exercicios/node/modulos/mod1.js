a5e703c15b7ea349b3544bd72bea58ccc48405bc
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-04-26 19:51:21 -0300

    Inicio modulo de NodeJs

---
+++ Exercicios/node/modulos/mod1.js
@@ -0,0 +1,15 @@
+const nome = 'Luis Felipe';
+const sobrenome = 'Salvarani';
+
+const falaNome = () => nome + ' ' + sobrenome;
+
+// module.exports.nome = nome;
+// module.exports.sobrenome = sobrenome;
+// module.exports.falaNome = falaNome;
+
+exports.nome = nome;
+exports.sobrenome = sobrenome;
+exports.falaNome = falaNome;
+this.qualquerCoisa = 'O que eu quiser exportar';
+
+console.log(module.exports);
