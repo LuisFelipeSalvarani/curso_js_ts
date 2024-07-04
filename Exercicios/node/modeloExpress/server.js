5f47ab6040e54f8de85473006ab878dd78a60515
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-07-04 16:15:50 -0300

    Middlewares

--- Exercicios/node/modeloExpress/server.js
+++ Exercicios/node/modeloExpress/server.js
@@ -2,16 +2,17 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const path = require('path');
-const middlewareGlobal = require('./src/middleware/middleware');
+const { middlewareGlobal, outroMiddleware } = require('./src/middleware/middleware');
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
-app.set('views engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.use(middlewareGlobal);
+app.use(outroMiddleware);
 app.use(routes);
 
 app.listen(3000, () => {
--- Exercicios/node/modeloExpress/src/middleware/middleware.js
+++ Exercicios/node/modeloExpress/src/middleware/middleware.js
@@ -1,5 +1,11 @@
-module.exports = (req, res, next) => {
+exports.middlewareGlobal = (req, res, next) => {
     console.log();
     console.log('Passei no middleware global');
     console.log();
+    next()
+}
+
+exports.outroMiddleware = (req, res, next) => {
+    console.log("Sou seu outro middleware")
+    next()
 }
